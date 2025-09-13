b0eba052a6be5d75517196846f32ac65f1c31212
Levi Adler <levialbuquerque05@gmail.com>
2025-09-12 23:13:55 -0300

    🎨 feat(views): integra botão de importação de animes na interface

--- appanime/views.py
+++ appanime/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
+from .models import Anime
+from .utils import importar_animes
 
-# Create your views here.
+def anime_list(request):
+    animes = Anime.objects.all()
+    return render(request, "appanime/anime_list.html", {"animes": animes})
+
+def anime_detail(request, pk):
+    anime = get_object_or_404(Anime, pk=pk)
+    return render(request, "appanime/anime_detail.html", {"anime": anime})
+
+def importar_animes_view(request):
+    query = request.GET.get("query", "naruto")
+    limit = int(request.GET.get("limit", 5))
+    salvos = importar_animes(query=query, limit=limit)
+    return JsonResponse({"salvos": salvos, "total": len(salvos)})
